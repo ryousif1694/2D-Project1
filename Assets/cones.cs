f2e33760b56857717a991b937846ca82f1d1df20
ryousif1694 <ryousif1694@sdsu.edu>
2025-10-27 10:07:54 -0700

    Update cones.cs

--- Assets/cones.cs
+++ Assets/cones.cs
@@ -5,30 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class Cones : MonoBehaviour
 {
-
+    //this timer is made so there cones would start after three seconds of game play 
     [SerializeField] float timer = 3f;
-   
+   //the movementOfItem is initialized to 3 to be the speed of the cones's movmeent 
     public float movementOfItem = 3f;
+    //cone prefab game object 
     public GameObject conePre;
-    public GameObject trophyPre;
-    public float changing = 2f;
-    public float itemExist = 2f;
 
     CurrentScore cscore;
     BestScore bscore;
+    //coneTouch is made and intilaized to 0 because the game starts at 0 being touched 
     public int coneTouch = 0;
+    //the maximum amount that the cone can be touch is 3 where once it reaches three times, the game would restart
     public int coneMaxCountTouch = 3;
-    public GameObject scoreGO;
-    // Start is called before the first frame update
+    public GameObject score;
+    //this is the cameraBoundary intialized to -10 so the ball, cones and trophy would be shown and not go passed this 
+    public float cameraBoundary = -10f;
+
     private void Start()
     {
-        cscore = scoreGO.GetComponent<CurrentScore>();
+        //this would reterieve the current score of the game from the start of the game 
+        cscore = score.GetComponent<CurrentScore>();
     }
-    public float cameraBoundary = -10f;
-    
 
     void Update()
     {
+        
         Vector3 currentPosition = transform.position;
         timer -= Time.deltaTime;
         if (timer <= 0)
@@ -51,6 +53,8 @@ public class Cones : MonoBehaviour
         cscore.current -= 100;
         cscore.setText();
         coneTouch += 1;
+
+
         
         if (coneTouch >= coneMaxCountTouch)
         {
