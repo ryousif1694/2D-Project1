3e7778a2a83ad62aa260b513d7ec1ef7d5c7e1ad
ryousif1694 <ryousif1694@sdsu.edu>
2025-10-28 01:54:18 -0700

    update best score

--- Assets/BestScore.cs
+++ Assets/BestScore.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 
 public class BestScore : MonoBehaviour
 {
+    //this is a Text object made to reference the TextMeshProUGUI 
     static private TextMeshProUGUI Text;
+    //this is the never changing score that would be 
     static private int point = 100;
-    private TextMeshProUGUI txtCom;
+ 
     public bool resetBestScore = false;
     void Awake()
         {
+            //The component of the TextMeshProUGUI is assigned to Text object 
             Text = GetComponent<TextMeshProUGUI>();
-
+            //checks if the 
             if (PlayerPrefs.HasKey("BestScore"))
             {
                 result = PlayerPrefs.GetInt("BestScore");
@@ -22,11 +25,14 @@ public class BestScore : MonoBehaviour
         }
         static public int result{
             get { 
+            //this would reterieve and rteurn the point there are 
             return point;
         }
             private set
             {
+                //point would initialized to value where in set it contains the numbers that are given to point in a variable way 
                 point = value;
+                //this would use 
                 PlayerPrefs.SetInt("BestScore", value);
 
                 if (Text!= null)
@@ -38,10 +44,13 @@ public class BestScore : MonoBehaviour
 
         static public void TRY_SET_BEST_SCORE(int goalScore)
         {
+        //this condition would check if the result is greater than the score that the player is aiming for called goalScore, then return
           if (result >= goalScore)
          {
-            return;
+            //this would get out of this condition 
+            return ;
          }
+          //and the goalScore would be assigned to result or else best score would be stuck at 0
             result = goalScore;
          }
         
